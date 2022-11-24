74bd726611fe2b4a5cf7b4bd6ce027732191a6e5
Yago Elias <yagoelias024@gmail.com>
2022-11-24 13:00:12 -0300

    user input
    Only accept letter input

--- periodo-2/linguagem-programacao-1/hangman.c
+++ periodo-2/linguagem-programacao-1/hangman.c
@@ -4,16 +4,15 @@
 #include <ctype.h>
 #include <time.h>
 
-typedef enum {WRONG_WORD, CORRECT_WORD, REPEATED_LETTER, UNREPEATED_LETTER, 
-LETTER_FOUND, LETTER_NOT_FOUND, ERROR_OPEN_FILE, WORD_LENGTH=12, SCREEN_SIZE=40} config;
+typedef enum {WRONG_WORD, CORRECT_WORD, REPEATED_LETTER, NOT_LETTER, UNREPEATED_LETTER, 
+LETTER_FOUND, IS_LETTER, LETTER_NOT_FOUND, ERROR_OPEN_FILE, WORD_LENGTH=12, SCREEN_SIZE=40} config;
 
 void initialize(char word[], int size);
 void execute();
 int found_letter(char secret_word[], char word[], char letter);
 void show_letters(char word[]);
-int repeated_letter(char typed_letters[], char letter);
-void print_error(char error_message[][21], int error_code);
-void screen(char word[], int attempts);
+int check_input(char typed_letters[], char letter);
+void screen(char word[], int attempts, char game[][51], int error_code);
 void choose_word(char select_word[]);
 
 int main()
@@ -48,34 +47,27 @@ void show_letters(char word[])
     printf("\033[2;%dH", center_text);
     for (i = 0; i < size; i++)
         printf("\033[93m%2c", word[i]);
-    //printf("\033[m");
+    printf("\033[m");
 }
 
-int repeated_letter(char typed_letters[], char letter)
+int check_input(char typed_letters[], char letter)
 {
     int i = 0;
-    letter = toupper(letter);
-    while (typed_letters[i] != '\0')
+    if (ispunct(letter) != 0)
+        return NOT_LETTER;
+    else if (isdigit(letter) == 0)
     {
-        if (typed_letters[i] == letter)
-            return REPEATED_LETTER;
-        ++i;
+        letter = toupper(letter);
+        while (typed_letters[i] != '\0')
+        {
+            if (typed_letters[i] == letter)
+                return REPEATED_LETTER;
+            ++i;
+        }
     }
+    else
+        return NOT_LETTER;
     return UNREPEATED_LETTER;
-}
-
-void print_error(char error_message[][21], int error_code)
-{
-    int error = -1;
-    switch (error_code)
-    {
-    case REPEATED_LETTER:
-        error = 0;
-        break;
-    case 3:
-        error = 1;
-        break;
-    }
 }
 
 void initialize(char word[], int size)
@@ -86,15 +78,22 @@ void initialize(char word[], int size)
     word[i] = '\0';
 }
 
-void screen(char word[], int attempts)
+void screen(char word[], int attempts, char game[][51], int error_code)
 {
+    int size, center_text;
+    size = strlen(game[error_code]);
+    center_text = (SCREEN_SIZE - size) / 2;
+
     printf("\033[1J \033[1;1H");
     printf(
         "\033[34m╒════════════════════════════════════════╕\033[m\n"
         "\033[34m╎                                        ╎\033[m\n"
         "\033[34m╘════════════════════════════════════════╛\033[m\n");
     show_letters(word);
-    printf("\n\n TRY [%02d/10] ", attempts);
+    
+    if (error_code == REPEATED_LETTER || error_code == NOT_LETTER)
+        printf("\033[1;%dH\033[34m╡ %s \033[34m╞", center_text, game[error_code]);
+    printf("\033[4;2H\033[93mTRY [%02d/10]\033[m ", attempts);
 }
 
 void choose_word(char secret_word[])
@@ -118,12 +117,15 @@ void choose_word(char secret_word[])
 void execute()
 {
     srand(time(NULL));
-    char secret_word[15], word[15], typed_letters[26] = {"\0"}, letter;
+    char letter;
+    char secret_word[15], word[15], typed_letters[22] = {"\0"};
 	char game[][51] = {
-        "\033[91m--------------  YOU LOSE ---------------\033[m", 
-        "\033[92m--------------  YOU WIN ----------------\033[m", 
+        "\033[91m---------------- YOU LOSE ----------------\033[m", 
+        "\033[92m---------------- YOU WIN -----------------\033[m", 
+        "\033[93mrepeated letter\033[m", 
+        "\033[93mnot is letter\033[m", 
+        "\033[93mtype only one letter\033[m"
     };
-	char error[][21] = {{"repeated letter"}, {"type only one letter"}};
 	int attempts = 10, ind_letter = 0, result_game=WRONG_WORD;
 	config error_code;
 
@@ -134,20 +136,20 @@ void execute()
 	{
 	    do
 	    {
-    	    screen(word, attempts);
+    	    screen(word, attempts, game, error_code);
     	    scanf(" %c", &letter);
-    	    error_code = repeated_letter(typed_letters,  letter);
-	        print_error(error, error_code);
-	    }while (error_code == REPEATED_LETTER);
+    	    error_code = check_input(typed_letters,  letter);
+	    }while (error_code == REPEATED_LETTER || error_code == NOT_LETTER);
+        error_code = UNREPEATED_LETTER;
 
 	    typed_letters[ind_letter] = toupper(letter);
 	    ind_letter++;
 
-	    if (found_letter (secret_word, word, letter) == LETTER_NOT_FOUND)
+	    if (found_letter(secret_word, word, letter) == LETTER_NOT_FOUND)
 	        attempts -= 1;
-	    if (strcmp (secret_word, word) == 0)
+	    if (strcmp(secret_word, word) == 0)
 	        result_game = CORRECT_WORD;
 	}
-    screen(secret_word, attempts);
+    screen(secret_word, attempts, game, error_code);
     printf("\n%s\n", game[result_game]);
 }

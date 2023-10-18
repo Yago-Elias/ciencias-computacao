71a1681c1e49ced731c6f5964836bae5f99930ee
Yago-Elias <yagoelias024@gmail.com>
2023-10-18 03:05:03 -0300

    desenvolvendo a função de tratamento de erro

--- periodo-4/algoritmos-para-grafos/projeto maze/labirinto.c
+++ periodo-4/algoritmos-para-grafos/projeto maze/labirinto.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #include "../lista-adjacencia/list.h"
 
-#define VERSAO "1.0"
+#define _VERSAO "1.0"
+
+typedef enum Parametro {INVALIDO=1, DFS, BFS, HELP, VERSAO} parametro;
 
 Graph gerarLabirinto(int u, int d) {
 	Graph gph = GRAPHinit(u);
@@ -17,7 +20,7 @@ Graph gerarLabirinto(int u, int d) {
 }
 
 void versao() {
-	printf("%s\n", VERSAO);
+	printf("maze - versão %s - Escrito por Yago Elias e Yuri Venceslau\n", _VERSAO);
 }
 
 void ajuda() {
@@ -32,19 +35,47 @@ void ajuda() {
 		"  t\t\t\tvértice de destino para a busca\n"
 		"  --v, --version\texibe a versão atual do programa\n"
 		"  --h, --help\t\texibe esta ajuda\n"
-		"\nDocumentação completa em: <https://github.com/Yago-Elias/ciencias-computacao/periodo-4/algoritmo-para-grafos/projeto-maze>\n"
+		"\nDocumentação completa em: <https://github.com/Yago-Elias/ciencias-computacao/periodo-4/algoritmo-para-grafos/projeto-maze/>\n"
 	);
 }
 
-int erro(int argc, char **argv) {
-	return 1;
+parametro argumento(char *arg) {
+	if (arg == NULL) return INVALIDO;
+	else if (!strcmp(arg, "--help") || !strcmp(arg, "--h"))
+		return HELP;
+	else if (!strcmp(arg, "--version") || !strcmp(arg, "--v"))
+		return VERSAO;
+	else if (!strcmp(arg, "dfs"))
+		return DFS;
+	else if (!strcmp(arg, "bfs"))
+		return BFS;
+	
+	return INVALIDO;
+}
+
+void erro(int argc, char **argv) {
+	parametro err = 0, opc;
+	opc = argumento(argv[1]);
+
+	if (argc != 2 && argc != 6) {
+		err = INVALIDO;
+		ajuda();
+	}
+	else if (argc == 2 && opc == INVALIDO) {
+		err = INVALIDO;
+		printf("Parâmetro '%s' inválido\nTente \"./maze --help\" para mais informações\n", argv[1]);
+	}
+	else if (argc == 6) {
+		int ql, qc;
+		err = INVALIDO;
+		ql = !isdigit(argv[1]) ? 1 : !isdigit(argv[2]) ? 2 : 0;
+		printf("%d\n", ql);
+		if (ql) printf("Parâmetro '%s' não é um valor inteiro\n", argv[ql]);
+	}
+
+	if (err) exit(err);
 }
 
 void run(int argc, char **argv) {
-	Graph gph = gerarLabirinto(atoi(argv[1]), atoi(argv[2]));
-
-	if (!strcmp(argv[1], "--h") || !strcmp(argv[1], "--help"))
-		GRAPHdfs(gph);
-	else
-		GRAPHbfs(gph, 0);
+	erro(argc, argv);
 }

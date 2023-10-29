b8ace09fb8bec13dc854edc39214f98d44129894
yurivenceslau <gamesclayman@gmail.com>
2023-10-29 17:34:04 -0300

    delecão dos arquivos de lista e fila fora da pasta lib

--- periodo-4/algoritmos-para-grafos/projeto maze/mainTeste.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "../lista-adjacencia/list.h"
-#define _VERSAO "1.0"
-
-int main(){
-    
-}
--- periodo-4/algoritmos-para-grafos/projeto maze/queue.h
+++
@@ -1,8 +0,0 @@
-#define vertex int
-
-void QUEUEinit(int N);
-int QUEUEempty(void);
-void QUEUEput(vertex v);
-vertex QUEUEget(void);
-void QUEUEfree(void);
-void QUEUEprint(void);
